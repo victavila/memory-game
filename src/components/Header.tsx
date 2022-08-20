937dd016247a97db3cac256aa8ff96413b0c2a07
Victor Avila <victoravila1997@gmail.com>
2022-08-19 21:19:13 -0500

    Added score header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
-const Header = () => {
+
+interface HeaderProps {
+  score: number;
+  bestScore: number;
+}
+
+const Header = ({ score, bestScore}: HeaderProps) => {
   return (
     <div className="header">
       <h1 className="title">Memory Card Game</h1>
       <div className="score">
-        <div className="current-score">Score: </div>
-        <div className="best-score">Best Score: </div>
+        <div className="current-score">Score: {score}</div>
+        <div className="best-score">Best Score: {bestScore}</div>
       </div>
     </div>
   )
